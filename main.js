66bace62948b7846dddcd5987fcd2d0dc1cb1deb
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-03-26 19:24:47 +0330

    feat: develop mobile nav section

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 import './src/Assets/Styles/index.scss'
+
+let backdrop = document.querySelector(".backdrop");
+let toggleButton = document.querySelector(".mobileNavBtn");
+let mobileNav = document.querySelector(".mobileNav");
+
+backdrop.addEventListener("click", function() {
+  mobileNav.classList.remove("open");
+  backdrop.classList.remove("open");
+  toggleButton.classList.remove("active", "change");
+});
+
+toggleButton.addEventListener("click", function() {
+  mobileNav.classList.add("open");
+  toggleButton.classList.add("active", "change");
+  backdrop.classList.add("open");
+});
