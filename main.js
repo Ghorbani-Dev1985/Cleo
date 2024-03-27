27c3a431d6918e93162c467f8a95f274adf0c592
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-03-27 18:44:48 +0330

    feat: develop add link to menu

--- main.js
+++ main.js
@@ -3,11 +3,16 @@ import './src/Assets/Styles/index.scss'
 let backdrop = document.querySelector(".backdrop");
 let toggleButton = document.querySelector(".mobileNavBtn");
 let mobileNav = document.querySelector(".mobileNav");
+let mobileNavLi = document.querySelectorAll(".mobileNavLi");
 
-backdrop.addEventListener("click", function() {
+function closeHandler(){
   mobileNav.classList.remove("open");
   backdrop.classList.remove("open");
   toggleButton.classList.remove("active", "change");
+}
+
+backdrop.addEventListener("click", function() {
+  closeHandler()
 });
 
 toggleButton.addEventListener("click", function() {
@@ -15,3 +20,7 @@ toggleButton.addEventListener("click", function() {
   toggleButton.classList.add("active", "change");
   backdrop.classList.add("open");
 });
+
+mobileNavLi.forEach((li) => {
+  li.addEventListener("click" , closeHandler)
+})
